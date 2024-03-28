5b1a9a7b9b2c8fe7cd9bb797ad6a557cbeee4182
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-03-28 17:37:10 -0400

    Se completó la page perfil de usuario

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,6 +1,26 @@
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
 
 export const DashProfile = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Perfil de Usuario</h1>
+      <form className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+          <img src={currentUser.profilePicture} alt='user' className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
+        </div>
+        <TextInput type='text' id='username' placeholder='Escribe tu nombre' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='Escribe tu correo electrónico' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='Escribe una nueva contraseña'/>
+        <Button type='submit' gradientDuoTone='greenToBlue' outline>
+          Guardar cambios
+        </Button>
+      </form>
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span className='cursor-pointer'>Eliminar cuenta</span>
+        <span className='cursor-pointer'>Cerrar Sesión</span>
+      </div>
+    </div>
   )
 }
