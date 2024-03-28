567d54efaca1734ff50429fcb0462e064641fe82
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-03-28 15:54:54 -0400

    Se realizó la ruta privada para el panel de control de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
-import { Projects } from './pages/Projects';
-import { Dashboard } from './pages/Dashboard';
-import { Header } from './components/Header';
-import { FooterComponent } from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Projects } from "./pages/Projects";
+import { Dashboard } from "./pages/Dashboard";
+import { Header } from "./components/Header";
+import { FooterComponent } from "./components/Footer";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/iniciar-sesion' element={<SignIn />} />
-        <Route path='/registrarse' element={<SignUp />} />
-        <Route path='/panel-control' element={<Dashboard />} />
-        <Route path='/proyectos' element={<Projects />} />
-        <Route path='/acerca-de' element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/iniciar-sesion" element={<SignIn />} />
+        <Route path="/registrarse" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/panel-control" element={<Dashboard />} />
+        </Route>
+        <Route path="/proyectos" element={<Projects />} />
+        <Route path="/acerca-de" element={<About />} />
       </Routes>
       <FooterComponent />
     </BrowserRouter>
-  )
-}
+  );
+};
