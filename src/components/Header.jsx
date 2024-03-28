242867cbb9d704c15d47d92cc527674e97eb4f1f
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-03-28 09:36:52 -0400

    Se actualizó el componente Header con los datos de usuario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux'
+
 export const Header = () => {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector(state => state.user);
   return (
     <Navbar className='border-b-2'>
       <Link to='' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white '>
@@ -25,21 +28,49 @@ export const Header = () => {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/iniciar-sesion'>
-          <Button gradientDuoTone='greenToBlue' outline>
-            Iniciar Sesión
-          </Button>
-        </Link>
+        {
+          currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                  alt='user'
+                  img={currentUser.profilePicture}
+                  rounded
+                />
+              }>
+              <Dropdown.Header>
+                <span className='block text-sm'>@{currentUser.username}</span>
+                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+              </Dropdown.Header>
+              <Link to='/panel-control?tab=perfil'>
+                <Dropdown.Item>Perfil</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Link to='/iniciar-sesion'>
+                <Dropdown.Item>Cerrar Sesión</Dropdown.Item>
+              </Link>
+            </Dropdown>
+          ) : (
+            <Link to='/iniciar-sesion'>
+              <Button gradientDuoTone='greenToBlue' outline>
+                Iniciar Sesión
+              </Button>
+            </Link>
+          )
+        }
+
         <NavbarToggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={ path === '/' } as={'div'}>
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Principal</Link>
         </Navbar.Link>
-        <Navbar.Link active={ path === '/proyectos' } as={'div'}>
+        <Navbar.Link active={path === '/proyectos'} as={'div'}>
           <Link to='/proyectos'>Proyectos</Link>
         </Navbar.Link>
-        <Navbar.Link active={ path === '/acerca-de' } as={'div'}>
+        <Navbar.Link active={path === '/acerca-de'} as={'div'}>
           <Link to='/acerca-de'>Acerca de</Link>
         </Navbar.Link>
       </Navbar.Collapse>
