73d4e60b59b4215fac75af7200568d8824a5290c
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-03-28 10:22:19 -0400

    Se adicionó funcionalidad al modo dark

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice.js';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
+
+import userReducer from './user/userSlice.js';
+import themeReducer from './theme/themeSlice.js';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
