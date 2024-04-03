778a04d2268fa26c728f679d85fb7ff2adc34dff
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-03 09:53:01 -0400

    Se completó la funcionalidad para realizar la carga de una imagen en la page CreatePost

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,11 +1,58 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react'
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
+import { useState } from 'react';
+
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+
 export const CreatePost = () => {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Por favor, seleccione una imagen');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Carga de imagen Fallida');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError('Carga de imagen Fallida');
+      setImageUploadProgress(null);
+    }
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Crear un  Post</h1>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Crear Post</h1>
       <form className='flex flex-col gap-4'>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput type='text' placeholder='Escribir título' id='title' className='flex-1' required />
@@ -19,9 +66,27 @@ export const CreatePost = () => {
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-200 border-dotted p-3'>
-          <FileInput type='file' accept='image/*' />
-          <Button type='button' gradientDuoTone='redToYellow' size='sm' outline>Subir imagen</Button>
+          <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+          <Button type='button' gradientDuoTone='redToYellow' size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress}>
+            {
+              imageUploadProgress ? (
+                <div className='w-16 h-16'>
+                  <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`} />
+                </div>
+              ):(
+                'Subir imagen'
+              )
+            }
+          </Button>
         </div>
+        {
+          imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>
+        }
+        {
+          formData.image && (
+            <img src={formData.image} alt='upload' className='w-full h-72 object-cover' />
+          )
+        }
         <ReactQuill theme='snow' placeholder='Escribe detalles...' className='h-72 mb-12' />
         <Button type='submit' gradientDuoTone='purpleToBlue'>Publicar</Button>
       </form>
