3862dab96c7d571be7c13093cbc347ccbd21f6ec
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-05 09:17:00 -0400

    Se adicionó funcionalidad al boton eliminar post en el componente DashPost

--- src/components/DashPosts.jsx
+++ src/components/DashPosts.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Table } from 'flowbite-react';
+import { Button, Modal, Table } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] =useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -44,6 +47,24 @@ export const DashPosts = () => {
     }
   };
 
+  const handleDeletePost = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+        method: 'DELETE'
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete));
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {
@@ -70,7 +91,12 @@ export const DashPosts = () => {
                       </Table.Cell>
                       <Table.Cell><Link to={`/post/${post.slug}`} className='font-medium text-gray-900 dark:text-white'>{post.title}</Link></Table.Cell>
                       <Table.Cell>{post.category}</Table.Cell>
-                      <Table.Cell className='font-medium text-red-500 hover:underline cursor-pointer'>
+                      <Table.Cell className='font-medium text-red-500 hover:underline cursor-pointer'
+                        onClick={() => {
+                          setShowModal(true);
+                          setPostIdToDelete(post._id);
+                        }}
+                      >
                         Eliminar
                       </Table.Cell>
                       <Table.Cell>
@@ -95,6 +121,23 @@ export const DashPosts = () => {
           <p>No tienes posts</p>
         )
       }
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-500 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>¿Estas seguro que quieres eliminar este post?</h3>
+          </div>
+          <div className='flex justify-center gap-4'>
+            <Button color='failure' onClick={handleDeletePost}>
+              Si, estoy seguro
+            </Button>
+            <Button color='gray' onClick={() => setShowModal(false)}>
+              No, cancelar
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div >
-  )
+  );
 }
