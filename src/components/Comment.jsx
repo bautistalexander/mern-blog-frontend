adcaf010059b7f2327a35b48b41fb40d0df1613a
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-12 16:12:17 -0400

    Se adicionó el boton para editar el contenido de un comentario como también se le dió funcionalidad

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,9 +2,12 @@ import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { FaThumbsUp } from 'react-icons/fa'
 import { useSelector } from 'react-redux';
+import { Button, Textarea } from 'flowbite-react';
 
-export const Comment = ({ comment, onLike }) => {
+export const Comment = ({ comment, onLike, onEdit }) => {
   const [user, setUser] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(comment.content);
   const { currentUser } = useSelector((state) => state.user);
   // console.log(moment.locale());
   useEffect(() => {
@@ -21,6 +24,32 @@ export const Comment = ({ comment, onLike }) => {
     };
     getUser();
   }, [comment]);
+
+  const handleEdit = async () => {
+    setIsEditing(true);
+    setEditedContent(comment.content);
+  };
+
+  const handleSave = async () => {
+    try {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content: editedContent
+        })
+      });
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
       <div className='flex-shrink-0 mr-3'>
@@ -31,20 +60,47 @@ export const Comment = ({ comment, onLike }) => {
           <span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.username}` : 'Usuario Anónimo'}</span>
           <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <p className='text-gray-700 pb-2'>{comment.content}</p>
-        <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
-          <button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${
-              currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'
-            }`}
-          >
-            <FaThumbsUp className='text-sm' />
-          </button>
-          <p className='text-gray-400'>
-            {
-              comment.numberOfLikes > 0 && (comment.numberOfLikes + ' ' + (comment.numberOfLikes === 1 ? 'Me gusta' : 'Me gustas'))
-            }
-          </p>
-        </div>
+        {
+          isEditing ? (
+            <>
+              <Textarea className='mb-2'
+                value={editedContent}
+                onChange={(e) => setEditedContent(e.target.value)}
+              />
+              <div className='flex justify-end gap-2 text-xs'>
+                <Button type='button' size='sm' gradientDuoTone='tealToLime'
+                  onClick={handleSave}
+                >Guardar</Button>
+                <Button type='button' size='sm' gradientDuoTone='tealToLime' outline
+                  onClick={() => setIsEditing(false)}
+                >Cancelar</Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <p className='text-gray-700 pb-2'>{comment.content}</p>
+              <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+                <button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'
+                  }`}
+                >
+                  <FaThumbsUp className='text-sm' />
+                </button>
+                <p className='text-gray-400'>
+                  {
+                    comment.numberOfLikes > 0 && (comment.numberOfLikes + ' ' + (comment.numberOfLikes === 1 ? 'Me gusta' : 'Me gustas'))
+                  }
+                </p>
+                {
+                  currentUser && ((currentUser._id === comment.userId || currentUser.isAdmin) &&
+                    <button type='button' className='text-gray-400 hover:text-blue-500' onClick={handleEdit}>
+                      Editar
+                    </button>
+                  )
+                }
+              </div>
+            </>
+          )
+        }
       </div>
     </div>
   )
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -79,6 +79,12 @@ export const CommentSection = ({ postId }) => {
       console.log(error.message)
     }
   };
+
+  const handleEdit = async (comment, editedComment) => {
+    setComments(
+      comments.map((c) => c._id === comment._id ? {...c, content: editedComment} : c)
+    );
+  };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {
@@ -129,7 +135,7 @@ export const CommentSection = ({ postId }) => {
             </div>
             {
               comments.map(comment => (
-                <Comment key={comment._id} comment={comment} onLike={handleLike} />
+                <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
               ))
             }
           </>
