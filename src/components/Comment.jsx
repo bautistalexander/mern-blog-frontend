1e3dcde1638c46247c453d92606dd4eb745c0177
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-12 17:01:43 -0400

    Se adicionó el boton para eliminar el contenido de un comentario como también se le dió funcionalidad

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa'
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export const Comment = ({ comment, onLike, onEdit }) => {
+export const Comment = ({ comment, onLike, onEdit, onDelete }) => {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -50,6 +50,9 @@ export const Comment = ({ comment, onLike, onEdit }) => {
     }
   };
 
+  const handleDelete = async () => {
+
+  }
   return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
       <div className='flex-shrink-0 mr-3'>
@@ -78,7 +81,7 @@ export const Comment = ({ comment, onLike, onEdit }) => {
             </>
           ) : (
             <>
-              <p className='text-gray-700 pb-2'>{comment.content}</p>
+              <p className='text-gray-500 pb-2'>{comment.content}</p>
               <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
                 <button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'
                   }`}
@@ -92,9 +95,16 @@ export const Comment = ({ comment, onLike, onEdit }) => {
                 </p>
                 {
                   currentUser && ((currentUser._id === comment.userId || currentUser.isAdmin) &&
-                    <button type='button' className='text-gray-400 hover:text-blue-500' onClick={handleEdit}>
-                      Editar
-                    </button>
+                    <>
+                      <button type='button' className='text-gray-400 hover:text-blue-500' 
+                        onClick={handleEdit}>
+                        Editar
+                      </button>
+                      <button type='button' className='text-gray-400 hover:text-red-500' 
+                        onClick={() => onDelete(comment._id)}>
+                        Eliminar
+                      </button>
+                    </>
                   )
                 }
               </div>
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,14 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { Alert, Button, Textarea } from 'flowbite-react';
+import { Alert, Button, Textarea, Modal } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Comment } from './Comment';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState('');
   const [comments, setComments] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -67,11 +70,11 @@ export const CommentSection = ({ postId }) => {
       });
       if (res.ok) {
         const data = await res.json();
-        setComments(comments.map((comment) => 
-          comment._id === commentId ? { 
-            ...comment, 
-            likes: data.likes, 
-            numberOfLikes: data.likes.length 
+        setComments(comments.map((comment) =>
+          comment._id === commentId ? {
+            ...comment,
+            likes: data.likes,
+            numberOfLikes: data.likes.length
           } : comment
         ));
       }
@@ -82,8 +85,27 @@ export const CommentSection = ({ postId }) => {
 
   const handleEdit = async (comment, editedComment) => {
     setComments(
-      comments.map((c) => c._id === comment._id ? {...c, content: editedComment} : c)
+      comments.map((c) => c._id === comment._id ? { ...c, content: editedComment } : c)
     );
+  };
+
+  const handleDelete = async (commentId) => {
+    setShowModal(false);
+    try {
+      if (!currentUser) {
+        navigate('/iniciar-sesion');
+        return;
+      }
+      const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+        method: 'DELETE'
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(comments.filter((comment) => comment._id !== commentId));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -135,12 +157,38 @@ export const CommentSection = ({ postId }) => {
             </div>
             {
               comments.map(comment => (
-                <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
+                <Comment key={comment._id}
+                  comment={comment}
+                  onLike={handleLike}
+                  onEdit={handleEdit}
+                  onDelete={(comentId) => {
+                    setShowModal(true);
+                    setCommentToDelete(comentId);
+                  }}
+                />
               ))
             }
           </>
         )
       }
+
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-500 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>¿Estas seguro que quieres eliminar este comentario?</h3>
+          </div>
+          <div className='flex justify-center gap-4'>
+            <Button color='failure' onClick={() => handleDelete(commentToDelete)}>
+              Si, estoy seguro
+            </Button>
+            <Button color='gray' onClick={() => setShowModal(false)}>
+              No, cancelar
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
