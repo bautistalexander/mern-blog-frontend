7488eb011b4708d6cc2b190e41881cf6b566ffff
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-30 17:51:56 -0400

    Se completó el diseño de las pages Projects y About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,16 @@
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div >
+          <h1 className='text-3xl font font-semibold text-center  my-7'>A cerca de Alexander's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Este es un blog creado por Alexander Bautista el cual tiene como fin presentar diferentes contenidos de programación u otros temas referidos a la tecnología</p>
+            <p>Espero disfrutes el contenido de este blog</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,11 @@
+import { CallToAction } from '../components/CallToAction'
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Proyectos</h1>
+      <p className='text-md text-gray-500'>A continuación se muestran algunos proyectos</p>
+      <CallToAction />
+    </div>
   )
 }
