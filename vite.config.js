9e3b7f110d0331ec2f201fe052c814bfad59373d
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-06-06 21:58:36 -0400

    Se subio a un dominio

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blog-alexander.onrender.com', //lo cambie del http://localhost:3000 al que subie a render
+        changeOrigin: true, //tambien se adiciono esto
         secure: false
       }
     }, 
